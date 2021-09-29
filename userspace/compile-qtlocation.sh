18d65d32ac668d5451280c74a0447c0d836ab168
dinglx <ding@openpilot.net>
2021-09-29 12:34:54 +0800

    China github bug

--- userspace/compile-qtlocation.sh
+++ userspace/compile-qtlocation.sh
@@ -2,7 +2,7 @@
 
 # Build qtlocation with extra api responses parsed
 cd /tmp
-git clone https://github.com/commaai/qtlocation.git
+git clone git://github.com/commaai/qtlocation.git
 cd qtlocation
 git checkout d44ce6506d2dda542eb8e4b6fc06a6a6bf74bb48
 qmake
